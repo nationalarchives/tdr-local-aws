cd6483d296021e24018584fc9c6225854a54e3c8
Suzanne Hamilton <suzanne.hamilton@nationalarchives.gov.uk>
2020-05-21 17:47:16 +0100

    Enable cross-origin requests
    Enable all CORS requests, so that the frontend JavaScript can call
    this endpoint instead of the real Cognito endpoint.
    
    This is safe because this service will only ever be run in the local
    development environment.

--- build.sbt
+++ build.sbt
@@ -5,6 +5,7 @@ version := "0.1"
 scalaVersion := "2.13.2"
 
 libraryDependencies ++= Seq(
+  "ch.megard" %% "akka-http-cors" % "0.4.3",
   "com.typesafe.akka" %% "akka-http" % "10.1.12",
   "com.typesafe.akka" %% "akka-stream" % "2.6.3"
 )
--- src/main/scala/uk/gov/nationalarchives/tdr/localaws/cognito/Routes.scala
+++ src/main/scala/uk/gov/nationalarchives/tdr/localaws/cognito/Routes.scala
@@ -4,8 +4,10 @@ import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.server.Directives.complete
 import akka.http.scaladsl.server.Route
 
+import ch.megard.akka.http.cors.scaladsl.CorsDirectives._
+
 object Routes {
-  val route: Route = {
+  val route: Route = cors() {
     complete(StatusCodes.OK)
   }
 }
