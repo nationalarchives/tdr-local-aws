6e375263229cce8ba56e2afe326f724748467c2c
Suzanne Hamilton <suzanne.hamilton@nationalarchives.gov.uk>
2020-05-22 09:44:40 +0100

    Return a fake IdentityId
    This satisfies the first request from the frontend.

--- src/main/scala/uk/gov/nationalarchives/tdr/localaws/cognito/Routes.scala
+++ src/main/scala/uk/gov/nationalarchives/tdr/localaws/cognito/Routes.scala
@@ -1,13 +1,11 @@
 package uk.gov.nationalarchives.tdr.localaws.cognito
 
-import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.server.Directives.complete
 import akka.http.scaladsl.server.Route
-
 import ch.megard.akka.http.cors.scaladsl.CorsDirectives._
 
 object Routes {
   val route: Route = cors() {
-    complete(StatusCodes.OK)
+    complete("{\"IdentityId\": \"some-fake-identity-id\"}")
   }
 }
