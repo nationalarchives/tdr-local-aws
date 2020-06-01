461ff74419bf33eb9f2e38ab56e91433c5bed675
Suzanne Hamilton <suzanne.hamilton@nationalarchives.gov.uk>
2020-06-01 16:15:17 +0100

    Use Circe to serialize response to JSON
    This gives us more flexibility when defining new parameters in the response.

--- build.sbt
+++ build.sbt
@@ -4,8 +4,14 @@ version := "0.1"
 
 scalaVersion := "2.13.2"
 
+val circeVersion = "0.13.0"
+
 libraryDependencies ++= Seq(
   "ch.megard" %% "akka-http-cors" % "0.4.3",
   "com.typesafe.akka" %% "akka-http" % "10.1.12",
-  "com.typesafe.akka" %% "akka-stream" % "2.6.3"
+  "com.typesafe.akka" %% "akka-stream" % "2.6.3",
+  "de.heikoseeberger" %% "akka-http-circe" % "1.32.0",
+  "io.circe" %% "circe-core" % circeVersion,
+  "io.circe" %% "circe-generic" % circeVersion,
+  "io.circe" %% "circe-parser" % circeVersion
 )
--- src/main/scala/uk/gov/nationalarchives/tdr/localaws/cognito/Routes.scala
+++ src/main/scala/uk/gov/nationalarchives/tdr/localaws/cognito/Routes.scala
@@ -3,9 +3,14 @@ package uk.gov.nationalarchives.tdr.localaws.cognito
 import akka.http.scaladsl.server.Directives.complete
 import akka.http.scaladsl.server.Route
 import ch.megard.akka.http.cors.scaladsl.CorsDirectives._
+import de.heikoseeberger.akkahttpcirce.FailFastCirceSupport._
+import io.circe.generic.auto._
 
 object Routes {
   val route: Route = cors() {
-    complete("{\"IdentityId\": \"some-fake-identity-id\", \"Credentials\": {}}")
+    complete(CognitoResponse("some-fake-identity-id", Credentials()))
   }
 }
+
+case class CognitoResponse(IdentityId: String, Credentials: Credentials)
+case class Credentials()
