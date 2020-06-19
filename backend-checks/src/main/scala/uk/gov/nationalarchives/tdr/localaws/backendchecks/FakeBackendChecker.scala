0af9338e84d4c41899770b608c70ee899eaa6396
Suzanne Hamilton <suzanne.hamilton@nationalarchives.gov.uk>
2020-06-19 18:03:57 +0100

    Build the full path of a new file based on its actual parent directory
    This partially fixes a bug where all files paths were built relative to the
    original parent directory, not the actual directory of a new file.
    
    The bug fix only works for files in directories which existed when the app
    was started. The app currently crashes if you add a file to a newly created
    directory.

--- backend-checks/src/main/scala/uk/gov/nationalarchives/tdr/localaws/backendchecks/FakeBackendChecker.scala
+++ backend-checks/src/main/scala/uk/gov/nationalarchives/tdr/localaws/backendchecks/FakeBackendChecker.scala
@@ -1,9 +1,10 @@
 package uk.gov.nationalarchives.tdr.localaws.backendchecks
 
 import java.nio.file.StandardWatchEventKinds.ENTRY_CREATE
-import java.nio.file.attribute.BasicFileAttributes
 import java.nio.file._
+import java.nio.file.attribute.BasicFileAttributes
 
+import scala.collection.mutable
 import scala.jdk.CollectionConverters._
 
 object FakeBackendChecker extends App {
@@ -11,16 +12,18 @@ object FakeBackendChecker extends App {
   val parentDirectory = Paths.get("/tmp/test-data")
   val watcher = FileSystems.getDefault.newWatchService
 
-  registerAll(parentDirectory)
+  val initialPaths = registerAll(parentDirectory)
 
   while(true) {
     println("Watching for changes")
 
     val watchKey: WatchKey = watcher.take()
+    val eventPath = initialPaths(watchKey)
+
     watchKey.pollEvents().asScala.foreach((event: WatchEvent[_]) => {
       val pathEvent: WatchEvent[Path] = event.asInstanceOf[WatchEvent[Path]]
       val newFileName = pathEvent.context()
-      val fullPath = parentDirectory.resolve(newFileName)
+      val fullPath = eventPath.resolve(newFileName)
 
       if (Files.isDirectory(fullPath)) {
         registerAll(fullPath)
@@ -44,15 +47,20 @@ object FakeBackendChecker extends App {
     println(s"Placeholder for file checks on path $path")
   }
 
-  def registerAll(start: Path): Unit = {
+  def registerAll(start: Path): Map[WatchKey, Path] = {
+    val pathsByKey = mutable.Map.empty[WatchKey, Path]
+
     Files.walkFileTree(start, new SimpleFileVisitor[Path] {
       override def preVisitDirectory(directory: Path, attrs: BasicFileAttributes): FileVisitResult = {
         println(s"Registering directory $directory")
 
-        directory.register(watcher, ENTRY_CREATE)
+        val watchKey = directory.register(watcher, ENTRY_CREATE)
+        pathsByKey(watchKey) = directory
 
         FileVisitResult.CONTINUE
       }
     })
+
+    pathsByKey.toMap
   }
 }
