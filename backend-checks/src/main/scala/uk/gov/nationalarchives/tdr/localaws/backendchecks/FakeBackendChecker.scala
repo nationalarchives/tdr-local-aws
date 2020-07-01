d870857f20a3a9410f5e088b006879baa70a395b
Suzanne Hamilton <suzanne.hamilton@nationalarchives.gov.uk>
2020-07-01 13:35:49 +0100

    Send fake antivirus result to the API
    Currently, the antivirus response is always a "pass". Later, we will modify
    this to return a failure depending on the filename.

--- backend-checks/src/main/scala/uk/gov/nationalarchives/tdr/localaws/backendchecks/FakeBackendChecker.scala
+++ backend-checks/src/main/scala/uk/gov/nationalarchives/tdr/localaws/backendchecks/FakeBackendChecker.scala
@@ -3,6 +3,7 @@ package uk.gov.nationalarchives.tdr.localaws.backendchecks
 import java.nio.file._
 
 import com.typesafe.config.ConfigFactory
+import graphql.codegen.AddAntivirusMetadata.AddAntivirusMetadata
 import graphql.codegen.GetClientFileMetadata.getClientFileMetadata
 import uk.gov.nationalarchives.tdr.GraphQLClient
 import uk.gov.nationalarchives.tdr.localaws.backendchecks.auth.TokenService
@@ -16,9 +17,10 @@ object FakeBackendChecker extends App {
 
   private val apiUrl = config.getString("api.baseUrl")
   private val getDocumentClient = new GraphQLClient[getClientFileMetadata.Data, getClientFileMetadata.Variables](apiUrl)
+  private val antivirusClient = new GraphQLClient[AddAntivirusMetadata.Data, AddAntivirusMetadata.Variables](apiUrl)
 
   private val tokenService = new TokenService(config)
-  private val antivirusChecker = new AntivirusCheck(tokenService, getDocumentClient)
+  private val antivirusChecker = new AntivirusCheck(tokenService, getDocumentClient, antivirusClient)
 
   private val parentDirectory = Paths.get(config.getString("files.s3UploadDirectory"))
 
--- backend-checks/src/main/scala/uk/gov/nationalarchives/tdr/localaws/backendchecks/checks/AntivirusCheck.scala
+++ backend-checks/src/main/scala/uk/gov/nationalarchives/tdr/localaws/backendchecks/checks/AntivirusCheck.scala
@@ -3,6 +3,8 @@ package uk.gov.nationalarchives.tdr.localaws.backendchecks.checks
 import java.util.UUID
 
 import graphql.codegen.GetClientFileMetadata.getClientFileMetadata
+import graphql.codegen.AddAntivirusMetadata.AddAntivirusMetadata
+import graphql.codegen.types.AddAntivirusMetadataInput
 import uk.gov.nationalarchives.tdr.GraphQLClient
 import uk.gov.nationalarchives.tdr.localaws.backendchecks.auth.TokenService
 
@@ -10,14 +12,37 @@ import scala.concurrent.{ExecutionContext, Future}
 
 class AntivirusCheck(
                         tokenService: TokenService,
-                        getDocumentClient: GraphQLClient[getClientFileMetadata.Data, getClientFileMetadata.Variables]
+                        getDocumentClient: GraphQLClient[getClientFileMetadata.Data, getClientFileMetadata.Variables],
+                        antivirusClient: GraphQLClient[AddAntivirusMetadata.Data, AddAntivirusMetadata.Variables]
                       )(implicit val executionContext: ExecutionContext) extends FileCheck {
 
   def check(fileId: UUID): Future[Any] = {
     tokenService.token.flatMap(token => {
       val queryVariables = getClientFileMetadata.Variables(fileId)
 
-      getDocumentClient.getResult(token, getClientFileMetadata.document, Some(queryVariables))
+      getDocumentClient.getResult(token, getClientFileMetadata.document, Some(queryVariables)).flatMap(data => {
+        val originalPath = data.data match {
+          case Some(metadata) => metadata.getClientFileMetadata.originalPath.get
+          // TODO: Include original errors
+          case None => throw new RuntimeException("Error in GraphQL response")
+        }
+
+        val antivirusSoftware = "fake-local-antivirus"
+        val result = "fake-antivirus-result"
+        val fakeVersion = "1.0"
+        val fakeDate = 123l
+        val input = AddAntivirusMetadataInput(
+          fileId,
+          Some(antivirusSoftware),
+          Some(result),
+          Some(fakeVersion),
+          Some(fakeVersion),
+          Some(result),
+          fakeDate
+        )
+        val mutationVariables = AddAntivirusMetadata.Variables(input)
+        antivirusClient.getResult(token, AddAntivirusMetadata.document, Some(mutationVariables))
+      })
     })
   }
 }
