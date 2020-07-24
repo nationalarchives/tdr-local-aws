c93096127777e47e0372bbfb185032e222d9924d
Suzanne Hamilton <suzanne.hamilton@nationalarchives.gov.uk>
2020-07-24 13:50:09 +0100

    Move hard-coded S3 ninja keys into config file
    These keys still need to be hard-coded for the reasons given in the code comment,
    but it makes it easier to update or override them if they are stored in config.

--- cognito/src/main/scala/uk/gov/nationalarchives/tdr/localaws/cognito/Routes.scala
+++ cognito/src/main/scala/uk/gov/nationalarchives/tdr/localaws/cognito/Routes.scala
@@ -3,20 +3,16 @@ package uk.gov.nationalarchives.tdr.localaws.cognito
 import akka.http.scaladsl.server.Directives.complete
 import akka.http.scaladsl.server.Route
 import ch.megard.akka.http.cors.scaladsl.CorsDirectives._
+import com.typesafe.config.ConfigFactory
 import de.heikoseeberger.akkahttpcirce.FailFastCirceSupport._
 import io.circe.generic.auto._
 
 object Routes {
 
-  // Fake access key and secret key required to upload files to a local S3Ninja container. These values are fixed in the
-  // S3Ninja config: https://github.com/scireum/s3ninja/blob/master/src/main/resources/application.conf
-  //
-  // Ideally, they would be injected into this application and S3Ninja as environment variables, but there's no obvious
-  // way to set them in S3Ninja without building our own Docker image and modifying the contents of
-  // /home/sirius/app/application.conf. These values seem to be stable (they haven't been changed in 3 years) and are
-  // only used in a development environment, so committing them is not a security risk.
-  val localAccessKeyId = "AKIAIOSFODNN7EXAMPLE"
-  val localSecretKey = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
+  private val config = ConfigFactory.load
+
+  private val localAccessKeyId = config.getString("s3.accessKeyId")
+  private val localSecretKey = config.getString("s3.secretKey")
 
   val route: Route = cors() {
     complete(
